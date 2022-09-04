5b80637b5a88ceca4d1a6512a12fbc5d91d7f605
ENDER ESTEEVEN PARRA HERRERA <parraee@HILDEBOG.LOCAL>
2022-09-04 15:48:01 -0500

    carga de la actividad 2

---
+++ actividad_2/app/src/main/java/com/example/actividad_2/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.actividad_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.*;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView s_factorial,s_fibonicci;
+    private EditText v_entrada;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        s_factorial = findViewById(R.id.s_factorial);
+        s_fibonicci = findViewById(R.id.s_fibonicci);
+        v_entrada = findViewById(R.id.v_entrada);
+    }
+
+    public void factorial (View view)
+    {
+        int valor = Integer.parseInt(v_entrada.getText().toString());
+        int factorial = 1;
+
+        for (int i = 1; i <= valor; i++)
+        {
+            factorial= factorial * i;
+        }
+        s_factorial.setText(factorial+ " ");
+    }
+
+    public void fibonicci (View view)
+    {
+        int valor = Integer.parseInt(v_entrada.getText().toString());
+        int fibonicci = 1;
+        int num1=0;
+        int num2=1;
+
+        for (int i = 1; i <= valor; i++)
+        {
+            fibonicci= num1 + num2;
+            num1 = num2;
+            num2= fibonicci;
+        }
+        s_fibonicci.setText(fibonicci+ " ");
+    }
+}
